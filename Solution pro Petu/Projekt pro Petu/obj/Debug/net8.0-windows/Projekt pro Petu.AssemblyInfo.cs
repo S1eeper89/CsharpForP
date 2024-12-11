532c3e5f26f1fdb491faac8d0a40f32f5f6c8ac1
S1eper <hlahulek.jan@gmail.com>
2024-12-11 22:47:39 +0100

    T
    initial push

--- Solution pro Petu/Projekt pro Petu/obj/Debug/net8.0-windows/Projekt pro Petu.AssemblyInfo.cs
+++ Solution pro Petu/Projekt pro Petu/obj/Debug/net8.0-windows/Projekt pro Petu.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projekt pro Petu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70943a2b2a3446059dbedb4ca02dadd1732eddf8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+722d8bb224c74de7f64666de405b5b085727749f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projekt pro Petu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projekt pro Petu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
